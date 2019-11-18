d17bacf2d4459e981db51828abb6af69d5ff403f
Haneul Lee <lovesky4294@gmail.com>
2019-11-19 00:46:19 +0900

    room detail and 404 page

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("cores.urls", namespace="cores")),
+    path("rooms/", include("rooms.urls", namespace="rooms")),
     path("admin/", admin.site.urls),
 ]
 
--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render, redirect
 # from django.core.paginator import Paginator, EmptyPage
 from django.views.generic import ListView
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from . import models
 # from math import ceil
 
@@ -24,3 +26,11 @@ class HomeView(ListView):
     paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:  # 404 page
+        return redirect(reverse("cores:home"))
