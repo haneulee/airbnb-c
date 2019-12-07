b8a8b2c2ca97ee4435291f459b2f00379f2ed5ed
Haneul Lee <lovesky4294@gmail.com>
2019-12-07 20:22:42 +0900

    created login view

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include("cores.urls", namespace="cores")),
     path("rooms/", include("rooms.urls", namespace="rooms")),
+    path("users/", include("users.urls", namespace="users")),
     path("admin/", admin.site.urls),
 ]
 
