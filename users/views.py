fb20ed28e5e4283e4c856e62e73ab3ac1f904ec5
Haneul Lee <lovesky4294@gmail.com>
2019-12-07 20:43:09 +0900

    validating email and password

--- users/views.py
+++ users/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from . import forms
 
 
 class LoginView(View):
     def get(self, request):
-        return render(request, "users/login.html")
+        form = forms.LoginForm(initial={"email": "itn@las.com"})
+        return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
-        pass
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        return render(request, "users/login.html", {"form": form})
