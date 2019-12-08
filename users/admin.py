e5f4c1e13064719011ac73878768bcddc8a6ff72
Haneul Lee <lovesky4294@gmail.com>
2019-12-08 16:52:57 +0900

    sending verification email and complete

--- users/admin.py
+++ users/admin.py
@@ -42,6 +42,8 @@ class CustomUserAdmin(UserAdmin):  # UserAdmin을 CustomUserAdmin에서 쓸 수 
         "superhost",
         "is_staff",
         "is_superuser",
+        "email_verified",
+        "email_secret",
     )
 
 
--- users/models.py
+++ users/models.py
@@ -4,6 +4,9 @@ from django.conf import settings
 from django.db import models
 from django.core.mail import send_mail
 
+from django.utils.html import strip_tags
+from django.template.loader import render_to_string
+
 
 class User(AbstractUser):
     """ Custom User Model """
@@ -48,11 +51,16 @@ class User(AbstractUser):
         if self.email_verified is False:
             secret = uuid.uuid4().hex[:20]
             self.email_secret = secret
+            html_message = render_to_string(
+                "emails/verify_email.html", {"secret": secret}
+            )
             send_mail(
                 "Verify Airbnb Account",
-                f"Verify account, this is your secret: {secret}",
+                strip_tags(html_message),
                 settings.EMAIL_FROM,
                 [self.email],
                 fail_silently=False,
+                html_message=html_message,
             )
+            self.save()
         return
