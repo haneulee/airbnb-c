3c858398e88a865579f5b9fb815be14e78b3f8cd
Haneul Lee <lovesky4294@gmail.com>
2019-12-10 00:16:45 +0900

    get kakao profile image

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from . import forms, models
 import os
 import requests
+from django.core.files.base import ContentFile
 
 
 class LoginView(FormView):
@@ -175,6 +176,11 @@ def kakao_callback(request):
             )
             user.set_unusable_password()
             user.save()
+            if profile_image is not None:
+                photo_request = requests.get(profile_image)
+                user.avatar.save(
+                    f"{nickname}-avatar", ContentFile(photo_request.content)
+                )
         login(request, user)
         return redirect(reverse("core:home"))
     except KakaoException:
