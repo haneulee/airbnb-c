96335be56da2013769e119df361ccc9151859c31
Haneul Lee <lovesky4294@gmail.com>
2020-01-14 18:55:33 +0900

    fixed error (wsgi)

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,16 +8,14 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
 
-
-def application(environ, start_response):
-    if environ['mod_wsgi.process_group'] != '':
-        import signal
-        os.kill(os.getpid(), signal.SIGINT)
-    return ["killed"]
+# def application(environ, start_response):
+#     if environ['mod_wsgi.process_group'] != '':
+#         import signal
+#         os.kill(os.getpid(), signal.SIGINT)
+#     return ["killed"]
